7f7c6fab65c5f659bbb0fe57c22b4908f81d3bc1
Shunfa Xu <gouwa5700@gmail.com>
2024-10-12 23:11:47 -0700

    stopwatch demo (build failed)

--- app/src/main/java/com/example/servicedemo/MainActivity.kt
+++ app/src/main/java/com/example/servicedemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.servicedemo
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -11,15 +14,17 @@ import com.example.servicedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val serviceIntent = Intent(this, MyBackgroundService::class.java)
-        serviceIntent.putExtra(MyBackgroundService.NAME, "Alex")
-        serviceIntent.putExtra(MyBackgroundService.MARKS, 78)
+        serviceIntent = Intent(applicationContext, MyBackgroundService::class.java)
+        registerReceiver(updateTime, IntentFilter(MyBackgroundService.UPDATED_TIME))
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -28,13 +33,48 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnStart.setOnClickListener {
-            Log.i(MyBackgroundService.TAG, "Starting service")
-            startService(serviceIntent)
+            startOrStop()
         }
 
-        binding.btnStop.setOnClickListener {
-            Log.i(MyBackgroundService.TAG, "Stopping service")
-            stopService(serviceIntent)
+        binding.btnReset.setOnClickListener {
+            reset()
         }
+    }
+
+    private fun startOrStop() {
+        if (isStarted) {
+            stop()
+        } else {
+            start()
+        }
+    }
+
+    private fun start() {
+        Log.i(MyBackgroundService.TAG, "Starting service")
+        serviceIntent.putExtra(MyBackgroundService.CURRENT_TIME, time)
+        startService(serviceIntent)
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop() {
+        Log.i(MyBackgroundService.TAG, "Stopping service")
+        stopService(serviceIntent)
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset() {
+        stop()
+        time = 0.0
+        binding.tvTime.text = "00:00:00"
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(MyBackgroundService.CURRENT_TIME,0.0)
+            binding.tvTime.text = time.toString()
+        }
+
     }
 }
--- app/src/main/java/com/example/servicedemo/MyBackgroundService.kt
+++ app/src/main/java/com/example/servicedemo/MyBackgroundService.kt
@@ -4,31 +4,43 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import java.util.Timer
+import java.util.TimerTask
 
 class MyBackgroundService: Service() {
+    private val timer = Timer()
 
     init {
         Log.i(TAG, "Service has been created")
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.i(TAG, "Service started")
-        val name = intent?.getStringExtra(NAME)
-        val marks = intent?.getIntExtra(MARKS, 0)
-        Log.i(TAG, "Name: $name, Marks: $marks")
-        return START_STICKY
+        val time = intent.getDoubleExtra(CURRENT_TIME, 0.0)
+        timer.scheduleAtFixedRate(StopWatchTimerTask(time),0, 1000)
+        return START_NOT_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onDestroy() {
         Log.i(TAG, "Service Destorying...")
+        timer.cancel()
         super.onDestroy()
     }
 
     companion object{
         const val TAG = "MyTag"
-        const val NAME = "Name"
-        const val MARKS = "Total Marks"
+        const val CURRENT_TIME = "Current time"
+        const val UPDATED_TIME = "Updated time"
+    }
+
+    private inner class StopWatchTimerTask(private var time:Double): TimerTask(){
+        override fun run() {
+            val intent = Intent(UPDATED_TIME)
+            time++
+            intent.putExtra(CURRENT_TIME,time)
+            sendBroadcast(intent)
+        }
     }
 }
